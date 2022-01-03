a1f2571a82b9e25bfb54525e2bd1b258e44129ad
Simo Tuomisto <simo.tuomisto@aalto.fi>
2022-01-03 14:32:18 +0200

    fortran/hello-world: Adding fortran hello-world test

---
+++ fortran/hello-world/run_hello-world.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -l
+#SBATCH --mem=100M
+#SBATCH --time=00:05:00
+
+set -e
+
+if [[ "$#" -gt 0 ]]; then
+    MODULES=${@:1}
+    echo "Running on: "$(hostname)
+    echo "Loading modules: "$MODULES
+    module purge
+    module load $MODULES
+fi
+
+EXECUTABLE=hello-world
+
+if [[ -z "$COMPILER" ]]; then
+    COMPILER=$(command -v ifort || command -v gfortran)
+fi
+
+echo 'Compiling with '$COMPILER
+$COMPILER -o $EXECUTABLE hello-world.f90
+
+srun $EXECUTABLE | grep 'Hello world.'
