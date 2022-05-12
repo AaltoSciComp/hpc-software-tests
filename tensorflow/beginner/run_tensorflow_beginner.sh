eb8e4e3c31488c3a9569083968c8f1e34f61fae8
Simo Tuomisto <simo.tuomisto@aalto.fi>
2022-05-12 09:19:32 +0300

    tensorflow/beginner: Fixed download url and enabled unbuffered output

--- tensorflow/beginner/run_tensorflow_beginner.sh
+++ tensorflow/beginner/run_tensorflow_beginner.sh
@@ -17,7 +17,7 @@ EXAMPLE_SCRIPT=${EXAMPLE_NOTEBOOK/.ipynb/.py}
 
 if [ ! -e $EXAMPLE_NOTEBOOK ] ; then
     echo 'Downloading tensorflow beginner tutorial notebook ...'
-    curl -L -o $EXAMPLE_NOTEBOOK https://github.com/tensorflow/docs/raw/master/site/en/tutorials/quickstart/beginner.ipynb 
+    curl -L -o $EXAMPLE_NOTEBOOK https://raw.githubusercontent.com/tensorflow/docs/master/site/en/tutorials/quickstart/beginner.ipynb
 fi
 
 if [ ! -e $EXAMPLE_SCRIPT ] ; then
@@ -25,4 +25,4 @@ if [ ! -e $EXAMPLE_SCRIPT ] ; then
     ipython nbconvert $EXAMPLE_NOTEBOOK --to script --stdout > $EXAMPLE_SCRIPT
 fi
 
-python $EXAMPLE_SCRIPT
+python -u $EXAMPLE_SCRIPT
